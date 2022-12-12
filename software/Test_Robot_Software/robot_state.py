e4e71cd63138ba634a78da5e25305b2984274957
anish.shrestha <shr.anish00@gmail.com>
2022-12-12 15:20:51 +0200

    enum class with states added

--- software/Test_Robot_Software/robot_state.py
+++ software/Test_Robot_Software/robot_state.py
@@ -1,12 +1,10 @@
 import enum
 import numpy as np
 
+
 class State(enum.Enum):
-    INITIAL = 0
-    FIND_A_BALL = 1
-    FOLLOW_A_BALL = 2
+    MANUAL = 0
+    INITIAL = 1
+    FIND_A_BALL = 2
+    FOLLOW_A_BALL = 3   
 
-
-class StateMachine:
-    def __init__(self):
-        self.current_state = State.INITIAL
