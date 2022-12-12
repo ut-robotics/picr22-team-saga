9885dd4fa56d6bc24b1ef6c9821679c2afae1439
anish.shrestha <shr.anish00@gmail.com>
2022-12-12 22:34:26 +0200

    updated the mainboard communication program withs tate machine logic to drive forward on speed commands

--- software/Test_Robot_Software/mainboard_communication.py
+++ software/Test_Robot_Software/mainboard_communication.py
@@ -1,25 +1,5 @@
-import api
-import time
+from api import RobotMovement
 from robot_state import State
-
-# def main():
-#     try:
-#         while True:
-#             if (robot_state.StateMachine.current_state == robot_state.State.MANUAL):            
-#                 api.RobotMovement.move(0, 12, 0)       
-#                 print("Moving ..")
-
-#     except KeyboardInterrupt:
-#         print("Keyboard Interrupt ..")
-#         api.RobotMovement.move(0.5, 0.5, 0.5)
-#         print("Keyboard Interrupt ...")
-#         print("Closing ...")
-    
-#     finally:
-#         api.RobotMovement.close()
-
-# if __name__ == "__main__":
-#     main()
 
 class MainboardComm:
     """
@@ -34,16 +14,19 @@ class MainboardComm:
 
 if __name__ == "__main__":
     mainboard_comm = MainboardComm()
+    robot = RobotMovement()
     while True:
         try:
             if mainboard_comm.current_state == State.MANUAL:
-                api.RobotMovement.move(0, 12, 0)
+                robot.move(0, 15, 0)
 
         except KeyboardInterrupt:
-            api.RobotMovement.move(0, 0, 0)
+            robot.move(0, 0, 0)
             print("Keyboard Interrupt ...")
             print("Closing ...")
+            robot.close()
             break
 
-        finally:
-            api.RobotMovement.close()
+        # finally:
+        #     # robot.close()
+        #     pass
