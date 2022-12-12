1948f7f0b7f4c1a3a32edb7e9f372ce1709452a5
anish.shrestha <shr.anish00@gmail.com>
2022-12-12 15:58:14 +0200

    updated the mainboard communication with state machine implementation

--- software/Test_Robot_Software/mainboard_communication.py
+++ software/Test_Robot_Software/mainboard_communication.py
@@ -1,22 +1,49 @@
 import api
 import time
-import robot_state
+from robot_state import State
 
-def main():
-    try:
-        while True:
-            if (robot_state.StateMachine.current_state == robot_state.State.MANUAL):            
-                api.RobotMovement.move(0, 12, 0)       
-                print("Moving ..")
+# def main():
+#     try:
+#         while True:
+#             if (robot_state.StateMachine.current_state == robot_state.State.MANUAL):            
+#                 api.RobotMovement.move(0, 12, 0)       
+#                 print("Moving ..")
 
-    except KeyboardInterrupt:
-        print("Keyboard Interrupt ..")
-        api.RobotMovement.move(0.5, 0.5, 0.5)
-        print("Keyboard Interrupt ...")
-        print("Closing ...")
+#     except KeyboardInterrupt:
+#         print("Keyboard Interrupt ..")
+#         api.RobotMovement.move(0.5, 0.5, 0.5)
+#         print("Keyboard Interrupt ...")
+#         print("Closing ...")
     
-    finally:
-        api.RobotMovement.close()
+#     finally:
+#         api.RobotMovement.close()
+
+# if __name__ == "__main__":
+#     main()
+
+class MainboardComm:
+    """
+    This class is there just for veryfying everythin works
+    whena speed command is sent to the MCU
+    via serial
+    """
+
+    def __init__(self):
+        self.current_state = State.MANUAL
+    
 
 if __name__ == "__main__":
-    main()
+    mainboard_comm = MainboardComm()
+    while True:
+        try:
+            if mainboard_comm.current_state == State.MANUAL:
+                api.RobotMovement.move(0, 12, 0)
+
+        except KeyboardInterrupt:
+            api.RobotMovement.move(0, 0, 0)
+            print("Keyboard Interrupt ...")
+            print("Closing ...")
+            break
+
+        finally:
+            api.RobotMovement.close()
