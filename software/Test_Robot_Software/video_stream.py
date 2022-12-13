95de44ea66c0518a44953d3d2d3b2a6c339fc757
anish.shrestha <shr.anish00@gmail.com>
2022-12-13 14:12:07 +0200

    video streaming program wiht the largest ball fetched from processed data list without max filtering

--- software/Test_Robot_Software/video_stream.py
+++ software/Test_Robot_Software/video_stream.py
@@ -21,12 +21,12 @@ def main():
             
             # has argument aligned_depth that enables depth frame to color frame alignment. Costs performance
             processed_data = processor.process_frame(aligned_depth=False)
-            # largest = max(processed_data.balls, key = lambda ball: ball.size, default=None)
-            largest = processed_data.balls[-1]
-            if(largest):
-                cv2.circle(processed_data.debug_frame,(largest.x, largest.y), 20, (255, 0, 255), -1)
+            
+            if processed_data.balls:
+                largest = processed_data.balls[-1] 
+                cv2.circle(processed_data.debug_frame, (largest.x, largest.y), 20, (255, 0, 255), -1)
                 
-            print(largest)
+                print(largest)
 
             # This is where you add the driving behaviour of your robot. It should be able to filter out
             # objects of interest and calculate the required motion for reaching the objects
