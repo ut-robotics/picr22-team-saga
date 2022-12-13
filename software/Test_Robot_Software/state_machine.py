f30e399f606661ce1ad9d2c4dccd1674b22982ae
anish.shrestha <shr.anish00@gmail.com>
2022-12-13 18:35:26 +0200

    updated the state machine code with camera resolution parameters in the constructor

--- software/Test_Robot_Software/state_machine.py
+++ software/Test_Robot_Software/state_machine.py
@@ -9,9 +9,12 @@ class StateMachine:
     of the robot as implementing the state machine logic
     """
 
-    def __init__(self, robot_movement):
+    def __init__(self, robot_movement, frame_width, frame_height):
         self.robot_movement = robot_movement
         self.state = State.FIND_A_BALL
+        
+        self.frame_width = frame_width
+        self.frame_height = frame_height
         
         self.max_speed_x = 10
         self.max_speed_y = 40
@@ -24,15 +27,15 @@ class StateMachine:
     #     elif self.state == State.FOLLOW_A_BALL:
     #         self.follow_a_ball(ball_x, ball_y)
 
-    def run_current_state(self, processed_data, frame_width, frame_height):
+    def run_current_state(self, processed_data):
 
         if processed_data.balls:
             print("ball exists")
             largest = processed_data.balls[-1] 
             cv2.circle(processed_data.debug_frame, (largest.x, largest.y), 20, (255, 0, 255), -1)                
 
-            ball_x = (largest.x - frame_width/2) / (frame_width/2) 
-            ball_y = (frame_height/2 - largest.y) / (frame_height/2)
+            ball_x = (largest.x - self.frame_width/2) / (self.frame_width/2) 
+            ball_y = (self.frame_height/2 - largest.y) / (self.frame_height/2)
             print(ball_x, ball_y)
 
             if self.state == State.FIND_A_BALL:
