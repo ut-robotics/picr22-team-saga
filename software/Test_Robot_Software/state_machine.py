88fa6a864f87eab6ddf0a4a7b667f4475deb3ab9
anish.shrestha <shr.anish00@gmail.com>
2022-12-13 09:25:05 +0200

    state machine functions added

--- software/Test_Robot_Software/state_machine.py
+++ software/Test_Robot_Software/state_machine.py
@@ -12,7 +12,7 @@ class StateMachine:
         self.state = State.FIND_A_BALL
         self.counter = 0
 
-    def current_state(self, ball_x, ball_y):
+    def run_current_state(self, ball_x, ball_y):
 
         if self.state == State.FIND_A_BALL:
             self.find_a_ball(ball_x)
@@ -21,7 +21,37 @@ class StateMachine:
             self.follow_a_ball(ball_x, ball_y)
         
     def find_a_ball(self, ball_x):
-        pass
+        if ball_x is None:
+            self.robot_movement.move(0, 0, -8)
+        else:
+            self.state = State.FOLLOW_A_BALL
+            self.robot_movement.move(0, 0, 0)
 
     def follow_a_ball(self, ball_x, ball_y):
-        pass
+        
+        if ball_x is None:
+            self.state = State.FIND_A_BALL
+        else:
+        
+            max_speed_x = 10
+            max_speed_y = 30
+            
+            speed_x = ball_x * 18
+            if (speed_x > max_speed_x):
+                speed_x = max_speed_x
+            if (speed_x < -max_speed_x):
+                speed_x = -max_speed_x
+            
+            if (ball_y < -0.4):
+                ball_y = 0.0
+            speed_y = (ball_y + 0.4) * 60
+            if (speed_y > max_speed_y):
+                speed_y = max_speed_y
+            
+            rot_speed = -speed_x * 1.5
+            
+            self.robot_movement.move(speed_x, speed_y, rot_speed)
+        
+if __name__ == "__main__":
+    state_machine = StateMachine()
+    state_machine.robot_movement.move(0, 0, 0)
