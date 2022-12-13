fcf7325fd9aa3197ee853b1115a5ed395f800f69
anish.shrestha <shr.anish00@gmail.com>
2022-12-13 15:31:43 +0200

    refactored the code ommiting the redundant codes

--- software/Test_Robot_Software/state_machine.py
+++ software/Test_Robot_Software/state_machine.py
@@ -12,7 +12,7 @@ class StateMachine:
         self.state = State.FIND_A_BALL
         
         self.max_speed_x = 10
-        self.max_speed_y = 30
+        self.max_speed_y = 40
 
     def run_current_state(self, ball_x, ball_y):
         
@@ -23,39 +23,33 @@ class StateMachine:
             self.follow_a_ball(ball_x, ball_y)
         
     def find_a_ball(self, ball_x):
-        if ball_x is None:
-            self.robot_movement.move(0, 0, -8)
-        else:
-            self.state = State.FOLLOW_A_BALL
-            self.robot_movement.move(0, 0, 0)
+        
+        self.state = State.FOLLOW_A_BALL
+        self.robot_movement.move(0, 0, 0)
 
     def follow_a_ball(self, ball_x, ball_y):
-        
-        if ball_x is None:
-            self.state = State.FIND_A_BALL
-        else:                    
-            
-            speed_x = (ball_x - 0.225) * 16.0
 
-            if (speed_x > self.max_speed_x):
-                speed_x = self.max_speed_x
-            if (speed_x < -self.max_speed_x):
-                speed_x = -self.max_speed_x
+        speed_x = (ball_x - 0.225) * 16.0
+
+        if (speed_x > self.max_speed_x):
+            speed_x = self.max_speed_x
+        if (speed_x < -self.max_speed_x):
+            speed_x = -self.max_speed_x
+        
+        if (ball_y < -0.4):
+            speed_y = 0.0
             
-            if (ball_y < -0.4):
-                speed_y = 0.0
-                
-            else:                    
-                speed_y = (ball_y + 0.4)  * 60.0 
-            
-            if (speed_y > self.max_speed_y):
-                speed_y = self.max_speed_y
-            else:
-                pass
-            
-            rot_speed = -speed_x * 1.25
-            
-            self.robot_movement.move(speed_x, speed_y, rot_speed)
+        else:                    
+            speed_y = (ball_y + 0.4)  * 60.0 
+        
+        if (speed_y > self.max_speed_y):
+            speed_y = self.max_speed_y
+        else:
+            pass
+        
+        rot_speed = -speed_x * 1.25
+        
+        self.robot_movement.move(speed_x, speed_y, rot_speed)
 
     def turn(self, rot_speed):
         self.robot_movement.move(0, 0, rot_speed)    
