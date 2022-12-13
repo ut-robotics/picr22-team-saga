113401de2909bd174ad87f7404b2e6b7e61f2ee5
anish.shrestha <shr.anish00@gmail.com>
2022-12-13 09:25:43 +0200

    main file for all taks combnined

--- software/Test_Robot_Software/main.py
+++ software/Test_Robot_Software/main.py
@@ -2,6 +2,8 @@ from camera import RealsenseCamera
 from image_processor import ImageProcessor
 from api import RobotMovement
 from robot_state import State
+from state_machine import StateMachine
+from time import time
 
 import cv2
 
@@ -11,10 +13,63 @@ class Main:
     finding a ball, following a ball, etc
     """
 
-    def __init__(self):
-        self.cam = RealsenseCamera()
-        self.cam.open()
+    def __init__(self, robot_movement):
+
+        self.debug = True
+        self.cam = RealsenseCamera(exposure=100)
+        self.processor = ImageProcessor(self.cam, debug=self.debug)
+        self.processor.start()        
+
         self.robot_movement = RobotMovement()
-        
-        self.current_state = State.INITIAL
+        self.state_machine = StateMachine(self.robot_movement)
+        self.current_state = State.FIND_A_BALL
+
+if __name__ == "__main__":
+    main = Main()
+
+    start = time()
+    fps = 0
+    frame = 0
+    frame_cnt = 0
+
+    try:
+        while True:
+            processed_data = main.processor.process_frame(aligned_depth=False)
+            largest = processed_data.balls[-1]
+
+            if largest:
+                cv2.circle(processed_data.debug_frame, (largest.x, largest.y), 20, (255, 0, 255), -1)
+            
+            frame_cnt +=1
+
+            frame += 1
+            if frame % 30 == 0:
+                frame = 0
+                end = time.time()
+                fps = 30 / (end - start)
+                start = end
+                print("FPS: {}, framecount: {}".format(fps, frame_cnt))
+                print("ball_count: {}".format(len(processed_data.balls)))
+
+                #if (frame_cnt > 1000):
+                #    break
+
+            if main.debug:
+                debug_frame = processed_data.debug_frame
+
+                cv2.imshow('debug', debug_frame)
+
+                k = cv2.waitKey(1) & 0xff
+                if k == ord('q'):
+                    break
+    
+    except KeyboardInterrupt:
+        print("Keybaord Interrupt ..")
+        print("Closing ....")
+
+    finally:
+        main.processor.stop()
+        cv2.destroyAllWindows()
+                
+
         
