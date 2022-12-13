322467a4d2df0195fae06a1c80834668eca6540e
anish.shrestha <shr.anish00@gmail.com>
2022-12-13 16:19:38 +0200

    updated the state machine code which takes the entire processed data as input

--- software/Test_Robot_Software/state_machine.py
+++ software/Test_Robot_Software/state_machine.py
@@ -1,6 +1,8 @@
 from robot_state import State
 from api import RobotMovement
 
+import cv2
+
 class StateMachine:
     """
     This class is responsible for switching the states
@@ -14,14 +16,38 @@ class StateMachine:
         self.max_speed_x = 10
         self.max_speed_y = 40
 
-    def run_current_state(self, ball_x, ball_y):
+        self.FRAME_WIDTH = 848
+        self.FRAME_HEIGHT = 480
+
+
+
+    # def run_current_state(self, ball_x, ball_y):
         
-        if self.state == State.FIND_A_BALL:
-            self.find_a_ball(ball_x)
+    #     if self.state == State.FIND_A_BALL:
+    #         self.find_a_ball(ball_x)
         
-        elif self.state == State.FOLLOW_A_BALL:
-            self.follow_a_ball(ball_x, ball_y)
+    #     elif self.state == State.FOLLOW_A_BALL:
+    #         self.follow_a_ball(ball_x, ball_y)
+
+    def run_current_state(self, processed_data):
+
+        if processed_data.balls:
+            print("ball exists")
+            largest = processed_data.balls[-1] 
+            cv2.circle(processed_data.debug_frame, (largest.x, largest.y), 20, (255, 0, 255), -1)                
+
+            ball_x = (largest.x - self.FRAME_WIDTH/2) / (self.FRAME_WIDTH/2) 
+            ball_y = (self.FRAME_HEIGHT/2 - largest.y) / (self.FRAME_HEIGHT/2)
+            print(ball_x, ball_y)
+
+            if self.state == State.FIND_A_BALL:
+                self.find_a_ball(ball_x=ball_x)
+            elif self.state == State.FOLLOW_A_BALL:
+                self.follow_a_ball(ball_x=ball_x, ball_y=ball_y)
         
+        else:
+            self.robot_movement.move(0, 0, -8)
+            
     def find_a_ball(self, ball_x):
         
         self.state = State.FOLLOW_A_BALL
@@ -49,10 +75,7 @@ class StateMachine:
         
         rot_speed = -speed_x * 1.25
         
-        self.robot_movement.move(speed_x, speed_y, rot_speed)
-
-    def turn(self, rot_speed):
-        self.robot_movement.move(0, 0, rot_speed)    
+        self.robot_movement.move(speed_x, speed_y, rot_speed)  
     
         
 if __name__ == "__main__":
